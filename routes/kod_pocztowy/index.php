81402dd0da8877266ac4235a97a6430b79f2a3f8
root <root@localhost.localdomain>
2018-04-08 13:48:39 +0200

    Poprawa sprawdzania danych kodu pocztowego, poprawa funkcji sendData

--- routes/kod_pocztowy/index.php
+++ routes/kod_pocztowy/index.php
@@ -1,6 +1,6 @@
 <?php
 $result = [$_POST['name'] => 1];
 if (strlen($_POST['value']) < 1 || !preg_match('/^[0-9]{2}[-][0-9]{3}$/', $_POST['value'])) {
-    $result = 0;
+    $result = [$_POST['name'] => 0];
 }
 echo json_encode($result);
